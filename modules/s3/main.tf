37b0840e4eb85964b841c234cea9e98b3af374a6
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-07 17:43:02 -0300

    up: atualizando bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,9 +1,16 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.s3_bucket_name
   acl    = var.s3_acl
+}
 
-  website {
-    index_document = var.s3_site_index
-    error_document = var.s3_site_error
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = var.s3_site_error
+  }
+
+  error_document {
+    key = var.s3_site_index
   }
 }
