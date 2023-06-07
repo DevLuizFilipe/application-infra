7fa37313df810f46562b486e2344b3afd686fe9f
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-07 20:58:57 -0300

    ativando tfstate na nuvem

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
-# terraform {
-#   backend "s3" {
-#     bucket = module.bucket-tf.bucket_name
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = module.bucket-tf.bucket_name
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
