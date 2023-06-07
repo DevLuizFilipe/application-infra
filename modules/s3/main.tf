3397d904275d37cc218c33dec6c9835af5ad28cb
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-07 19:56:17 -0300

    fix: aplicando boas práticas

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,17 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.s3_bucket_name
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
