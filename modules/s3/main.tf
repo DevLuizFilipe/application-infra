7f0f01317a33f51bac26132beb84652309fe7e82
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-10 14:13:51 -0300

    aplicando correções

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,25 +1,12 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.s3_bucket_name
+  acl    = var.s3_bucket_acl
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "POST", "PUT"]
     allowed_origins = ["*"]
     expose_headers  = ["ETag"]
   }
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "PublicReadGetObject",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": ["s3:GetObject"],
-      "Resource": ["arn:aws:s3:::${var.s3_bucket_name}/*"]
-    }
-  ]
-}
-EOF
 }
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
