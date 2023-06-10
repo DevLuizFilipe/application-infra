b875b5b5d567db26d3c0f1c9f8587871b370b7d1
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-10 19:18:24 -0300

    teste

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,17 +16,17 @@ resource "aws_security_group" "security_group_ecs" {
   }
 
   ingress {
-    from_port       = "80"
-    to_port         = "80"
-    protocol        = "tcp"
-    security_groups = ["0.0.0.0/0"]
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port       = "443"
-    to_port         = "443"
-    protocol        = "tcp"
-    security_groups = ["0.0.0.0/0"]
+    from_port   = "443"
+    to_port     = "443"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
