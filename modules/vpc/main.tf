6552f067ef3d77d186da11c856e4220a7725bd0f
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-10 14:13:51 -0300

    aplicando correções

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,10 +9,10 @@ resource "aws_security_group" "security_group_ecs" {
   vpc_id = aws_vpc.vpc.id
 
   ingress {
-    from_port   = "0"
-    to_port     = "0"
-    protocol    = "-1"
-    cidr_blocks = [aws_security_group.security_group_elb.id]
+    from_port       = "0"
+    to_port         = "0"
+    protocol        = "-1"
+    security_groups = [aws_security_group.security_group_elb.id]
   }
   egress {
     from_port   = "0"
