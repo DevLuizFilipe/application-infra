f590f9d694130688b14b8f2a2447b32905ce4863
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-12 17:53:19 -0300

    alterando id

--- modules/elb/output.tf
+++ modules/elb/output.tf
@@ -3,6 +3,11 @@ output "target_group_arn" {
   description = "ARN do target group"
 }
 
+output "elb_id" {
+  value       = aws_lb.elb.id
+  description = "ID do load balancer"
+}
+
 output "dns_name" {
   value       = aws_lb.elb.dns_name
   description = "DNS do load balancer"
