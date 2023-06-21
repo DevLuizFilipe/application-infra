0a32d42c1a3a506b9e66c9d95d2ecdaf8d2c403e
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-21 00:51:20 -0300

    Revert "atualizando develop"
    This reverts commit 1ddb762bf5f013243ba818ae3f3831483a9ea15b.

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket = "tfstate-lab-application"
+    bucket = "tfstate-lab-waycarbon"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
