3db692c3094a2e3a1fa5947512bdb066baddadf9
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-21 00:51:20 -0300

    Revert "atualizando develop"
    This reverts commit 1ddb762bf5f013243ba818ae3f3831483a9ea15b.

---
+++ main.tf
@@ -0,0 +1,114 @@
+#Cria um bucket para o site estático
+module "bucket_website" {
+  source         = "./modules/s3/"
+  s3_bucket_name = "lab-application"
+  s3_site_index  = "index.html"
+  s3_site_error  = "error.html"
+}
+
+#Cria uma role para o ECS
+module "ecs_iam" {
+  source            = "./modules/iam/"
+  iam_ecs_role_name = "iam-ecs"
+}
+
+#Cria um cluster ECS
+module "ecs" {
+  source           = "./modules/ecs/"
+  ecs_cluster_name = "application"
+}
+
+#Cria uma task definition ECS
+module "application" {
+  source                       = "./modules/ecs_task/"
+  ecs_task_family              = "application"
+  ecs_task_role_arn            = module.ecs_iam.ecs_role_arn
+  ecs_task_compatibilities     = ["FARGATE"]
+  ecs_task_container_name      = "application"
+  ecs_task_container_image     = "luizfilipesm/waycarbon"
+  ecs_task_container_image_tag = "latest"
+  ecs_task_container_port      = "80"
+  ecs_task_port                = module.application.task_container_port
+  ecs_task_memory              = "512"
+  ecs_task_cpu                 = "256"
+  depends_on                   = [module.ecs_iam]
+}
+
+#Cria uma VPC com duas subnetes privadas que utiliza um NAT gateway e uma publica que utiliza um internet gateway
+module "vpc" {
+  source                   = "./modules/vpc/"
+  vpc_name                 = "vpc-application"
+  vpc_cidr_block           = "192.168.0.0/16"
+  vpc_subnet_private_count = "2"
+  vpc_subnet_public_count  = "2"
+  vpc_subnet_regions       = "us-east-1a,us-east-1b"
+}
+
+#Cria um load balancer
+module "elb" {
+  source                               = "./modules/elb/"
+  elb_name                             = "elb-application"
+  elb_type                             = "application"
+  elb_subnets                          = module.vpc.subnet_public_id
+  elb_security_groups                  = [module.vpc.security_group_id_elb]
+  elb_target_group_name                = "ecs-application-target-group"
+  elb_target_group_port                = module.application.task_container_port
+  elb_target_group_protocol            = "HTTP"
+  elb_target_group_vpc                 = module.vpc.vpc_id
+  elb_group_target_heatlh_interval     = "10"
+  elb_group_target_heatlh_path         = "/api"
+  elb_group_target_heatlh_timeout      = "5"
+  elb_group_target_heatlh_threshold    = "3"
+  elb_group_target_unhealthy_threshold = "2"
+  elb_listener_port_http               = "80"
+  elb_listener_protocol_http           = "HTTP"
+  depends_on                           = [module.vpc]
+}
+
+#Cria um service ECS
+module "application_service" {
+  source                       = "./modules/ecs_service/"
+  ecs_service_name             = "application"
+  ecs_service_cluster          = module.ecs.cluster_name
+  ecs_service_task             = module.application.task_arn
+  ecs_service_count            = "1"
+  ecs_service_type             = "FARGATE"
+  ecs_service_target_group_arn = module.elb.target_group_arn
+  ecs_service_container_name   = module.application.task_container_name
+  ecs_service_container_port   = module.application.task_container_port
+  ecs_service_subnets          = module.vpc.subnet_private_id
+  ecs_service_security_groups  = [module.vpc.security_group_id_ecs]
+  depends_on                   = [module.vpc, module.application, module.ecs]
+}
+
+#Cria um CDN para a aplicação
+module "cdn" {
+  source                  = "./modules/cdn/"
+  cdn_damin_name          = module.elb.dns_name
+  cdn_origin_id           = module.elb.elb_name
+  cdn_enabled             = "true"
+  cdn_ipv6                = "true"
+  cdn_allowed_methods     = ["GET", "HEAD", "OPTIONS"]
+  cdn_cached_methods      = ["GET", "HEAD", "OPTIONS"]
+  cdn_cache_target_origin = module.elb.elb_name
+  cdn_query_string        = "false"
+  cdn_cookies             = "none"
+  cdn_protocol_policy     = "redirect-to-https"
+  cdn_min_ttl             = "0"
+  cdn_default_ttl         = "3600"
+  cdn_max_ttl             = "86400"
+  cdn_geo_restriction     = "none"
+  cdn_certificate_default = "true"
+  cdn_target_path         = "/api"
+  depends_on              = [module.elb]
+}
+
+# #Cria um dominio e um record do tipo CNAME
+# module "route53" {
+#   source              = "./modules/route53/"
+#   route53_zone_name   = "labluizfilipe.com.br"
+#   route53_record_name = "application-teste"
+#   route53_record_type = "CNAME"
+#   route53_record_ttl  = "15"
+#   route53_records     = [""] #Alterar
+# }
